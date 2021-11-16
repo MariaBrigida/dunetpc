b75da1417ff7173110e06d450c6f521f37244c41
Jacob Calcutt <calcuttj@msu.edu>
2021-11-16 15:32:32 -0600

    VDColdboxPDSDecoder: changed the interpretation of EventHeader::length in order to not go over multiple waveforms. Added a debug flag for quieting output.

--- dune/VDColdbox/RawDecoding/VDColdboxPDSDecoder_module.cc
+++ dune/VDColdbox/RawDecoding/VDColdboxPDSDecoder_module.cc
@@ -49,11 +49,16 @@ public:
 
 private:
 
+  std::pair<raw::OpDetWaveform, recob::OpHit> MakeWaveformAndHit(
+      const dunedaq::dataformats::EventHeader * event_header, size_t data_pos,
+      uint8_t * data_ptr);
+
   hid_t fPrevStoredHandle = -1;
   hid_t fHDFFile = -1;
   std::string fOutputDataLabel;
   std::string fFileInfoLabel;
   bool fForceOpen;
+  bool fDebug;
 
   // Declare member data here.
 
@@ -64,7 +69,8 @@ dune::VDColdboxPDSDecoder::VDColdboxPDSDecoder(fhicl::ParameterSet const& p)
   : EDProducer{p},
     fOutputDataLabel{p.get<std::string>("OutputDataLabel")},
     fFileInfoLabel{p.get<std::string>("FileInfoLabel", "daq")},
-    fForceOpen(p.get<bool>("ForceOpen", false)) {
+    fForceOpen(p.get<bool>("ForceOpen", false)),
+    fDebug(p.get<bool>("Debug", false)) {
   produces<std::vector<raw::OpDetWaveform>>(fOutputDataLabel);
   produces<std::vector<recob::OpHit>>(fOutputDataLabel);
 }
@@ -86,9 +92,6 @@ void dune::VDColdboxPDSDecoder::produce(art::Event& e) {
   const std::string & file_name = infoHandle->GetFileName();
   hid_t file_id = infoHandle->GetHDF5FileHandle();
   
-  std::cout << "HDF5 FileName: " << file_name << std::endl;
-  std::cout << "Top-Level Group Name: " << group_name << std::endl;
-  
   //If the fcl file said to force open the file
   //(i.e. because one is just running DataPrep), then open
   //but only if we are on a new file -- identified by if the handle
@@ -104,19 +107,19 @@ void dune::VDColdboxPDSDecoder::produce(art::Event& e) {
 
   hid_t PDS_group = getGroupFromPath(fHDFFile, group_name + "/PDS");
   std::deque<std::string> region_names = getMidLevelGroupNames(PDS_group);
-  std::cout << "Got " << region_names.size() << " regions" << std::endl;
+  if (fDebug)
+    std::cout << "Got " << region_names.size() << " regions" << std::endl;
   for (const auto & n : region_names) {
-    std::cout << n << std::endl;
-
     hid_t region_group = getGroupFromPath(PDS_group, n);
     std::deque<std::string> element_names = getMidLevelGroupNames(region_group);
-    std::cout << "Got " << element_names.size() << " elements" << std::endl;   
+    if (fDebug)
+      std::cout << "Got " << element_names.size() << " elements" << std::endl;
     for (const auto & element_name : element_names) {
-      std::cout << element_name << std::endl;
+      if (fDebug) std::cout << element_name << std::endl;
 
       hid_t dataset = H5Dopen(region_group, element_name.data(), H5P_DEFAULT);
       hsize_t ds_size = H5Dget_storage_size(dataset);
-      std::cout << "\tDataset size: " << ds_size << std::endl;
+      if (fDebug) std::cout << "\tDataset size: " << ds_size << std::endl;
 
       std::vector<char> ds_data(ds_size);
       H5Dread(dataset, H5T_STD_I8LE, H5S_ALL, H5S_ALL, H5P_DEFAULT,
@@ -124,50 +127,34 @@ void dune::VDColdboxPDSDecoder::produce(art::Event& e) {
       H5Dclose(dataset);
 
       Fragment frag(&ds_data[0], Fragment::BufferAdoptionMode::kReadOnlyMode);
-      std::cout << "\tMade fragment" << std::endl;
-      std::cout << "\t" << frag.get_header() << std::endl;
+      if (fDebug) {
+        std::cout << "\tMade fragment" << std::endl;
+        std::cout << "\t" << frag.get_header() << std::endl;
+      }
 
       
       uint8_t * data_ptr = reinterpret_cast<uint8_t*>(frag.get_data());
-      size_t event_header_size = sizeof(EventHeader)/sizeof(unsigned int);
 
       size_t iP = 0;
       size_t data_pos = 0;
       while (data_pos < (frag.get_header().size - sizeof(FragmentHeader))) {
         EventHeader * event_header
             = reinterpret_cast<EventHeader*>(data_ptr + data_pos);
-        std::cout << "\tEvent header " << event_header->length << std::endl;
-
-        unsigned int nADC = (event_header->length - event_header_size)*2;
-        std::cout << "\tnADC: " << nADC << std::endl;
-        std::cout << "\t" << iP << std::endl;
-
-        unsigned long ts = 0;
-        for (unsigned int iword = 0; iword <= 3; ++iword) {
-          ts += ((unsigned long)(event_header->timestamp[iword])) << 16 * iword;
+        if (fDebug) {
+          std::cout << "\tEvent header " << event_header->length << std::endl;
+          std::cout << "\t" << iP << std::endl;
         }
 
-        //Need time, channel
-        raw::OpDetWaveform wf(ts, 0, nADC);
+        auto wf_hit = MakeWaveformAndHit(event_header, data_pos, data_ptr);
+        output_wfs->emplace_back(wf_hit.first);
+        output_hits->emplace_back(wf_hit.second);
 
-        //Iterate to the start of the adc data
-        unsigned short * adc_ptr = reinterpret_cast<unsigned short *>(
-            data_ptr + data_pos + event_header_size);
-
-        //unsigned short peak_time = 0;
-        //unsigned short max_adc = 0;
-
-        for (size_t i = 0; i < nADC; ++i) {
-          wf.push_back(*(adc_ptr + i));
-          //max_adc = std::max(max_adc, *(adc_ptr + i));
-          //if(max_adc == *adc) calpeaktime = idata;
-        }
-        output_wfs->emplace_back(wf);
-
+        //Iterate position in the data
         data_pos += event_header->length;
         ++iP;
       }
-      std::cout << "Iterated through " << iP << " packets " << std::endl;
+      if (fDebug) 
+        std::cout << "Iterated through " << iP << " packets " << std::endl;
     }
     H5Gclose(region_group);
   }
@@ -175,6 +162,39 @@ void dune::VDColdboxPDSDecoder::produce(art::Event& e) {
 
   e.put(std::move(output_wfs), fOutputDataLabel);
   e.put(std::move(output_hits), fOutputDataLabel);
+}
+
+std::pair<raw::OpDetWaveform, recob::OpHit>
+    dune::VDColdboxPDSDecoder::MakeWaveformAndHit(
+        const dunedaq::dataformats::EventHeader * event_header, size_t data_pos,
+        uint8_t * data_ptr) {
+
+  size_t nADC = (event_header->length - sizeof(dunedaq::dataformats::EventHeader))/2;
+  if (fDebug) std::cout << "\tnADC: " << nADC << std::endl;
+  unsigned long ts = 0;
+  for (unsigned int iword = 0; iword <= 3; ++iword) {
+    ts += ((unsigned long)(event_header->timestamp[iword])) << 16 * iword;
+  }
+
+  //Need time, channel
+  raw::OpDetWaveform wf(ts, event_header->group2, nADC);
+
+  //Iterate to the start of the adc data
+  unsigned short * adc_ptr = reinterpret_cast<unsigned short *>(
+      data_ptr + data_pos + sizeof(dunedaq::dataformats::EventHeader));
+
+  //size_t peak_tick = 0;
+  //unsigned short max_adc = 0;
+
+  for (size_t i = 0; i < nADC; ++i) {
+    wf.push_back(*(adc_ptr + i));
+    //max_adc = std::max(max_adc, *(adc_ptr + i));
+    //if(max_adc == *(adc_ptr + i)) peak_tick = i;
+  }
+  //std::cout << "Max adc, peak tick: " << max_adc << ", " << peak_tick <<
+  //             std::endl;
+
+  return {wf, recob::OpHit()};
 }
 
 DEFINE_ART_MODULE(dune::VDColdboxPDSDecoder)
